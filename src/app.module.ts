e5c2241422653e2caf11631c0626a412938ecfd6
kijeong <ykjmove@gmail.com>
2024-07-03 00:24:49 +0900

    chore : dockerize init - #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 import { AchievementModule } from './achievement/achievement.module';
 import { GameModule } from './game/game.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/entities/todo.entity';
 
 @Module({
-  imports: [UserModule, TodoModule, AchievementModule, GameModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_CONTAINER_NAME,
+      port: parseInt(process.env.MYSQL_TCP_PORT),
+      database: process.env.MYSQL_DATABASE,
+      entities: [Todo],
+
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_USER_PASSWORD,
+      synchronize: true,
+    }),
+    UserModule,
+    TodoModule,
+    AchievementModule,
+    GameModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = process.env.NEST_API_PORT || 3000;
+
+  await app.listen(PORT, () => {
+    console.log(`${PORT} running`);
+  });
 }
 bootstrap();
