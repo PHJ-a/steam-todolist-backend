1f7bbf15cee5e7c17654eea07815ffe2c69f0a3a
Heo Daewoong <princewoong@gmail.com>
2024-07-05 14:33:12 +0900

    refactor: delete useless files and codes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,18 +11,12 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
 
   @Get('status')
   @HttpCode(200)
