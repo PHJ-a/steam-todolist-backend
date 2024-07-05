83e09b35757e0a690501dd41384366cfdef1ce31
kijeong <ykjmove@gmail.com>
2024-07-05 14:11:48 +0900

    fix : api 응답값 수정 #15

--- src/todo/dto/res-todoDetail.dto.ts
+++ src/todo/dto/res-todoDetail.dto.ts
@@ -5,6 +5,7 @@ export class ResTodoDetailDto {
   gameName: string;
   gameId: number;
   achieveId: number;
+  achieveTag: string;
   achieveName: string;
   achieveDescription: string;
   achieveIcon: string;
@@ -14,7 +15,8 @@ export class ResTodoDetailDto {
     this.id = data.todo.id;
     this.gameName = data.game.name;
     this.gameId = data.game.appid;
-    this.achieveName = data.todo.achievement.name;
+    this.achieveTag = data.todo.achievement.name;
+    this.achieveName = data.todo.achievement.displayName;
     this.achieveDescription = data.todo.achievement.description;
     this.achieveIcon = data.todo.achievement.icon_gray;
     this.start = data.todo.start;
