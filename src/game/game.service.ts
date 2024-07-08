d9a6d02fd911823863b2a905743c4fb74d2b9d7d
kijeong <ykjmove@gmail.com>
2024-07-09 00:06:50 +0900

    add : 서비스 예외 처리 #20

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  ServiceUnavailableException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import axios from 'axios';
 import { Game } from './entities/game.entity';
@@ -16,13 +21,7 @@ export class GameService {
 
   /** db와 steam 게임 데이터 동기화 */
   async fetchGames(user: User) {
-    const steamApiKey = this.configService.get<string>('STEAM_API_KEY');
-    const { data } = await axios.get(
-      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${steamApiKey}&steamid=${user.steamid}&include_appinfo=1`,
-    );
-
-    /** 스팀에서 가져온 해당 유저가 보유한 게임 목록 */
-    const gamesFromSteam = data.response.games;
+    const gamesFromSteam = await this.getUserGamesFromSteam(user.steamid);
 
     /** db에 저장된 게임 목록 */
     const dbGames = await this.gameRepository.find({
@@ -39,51 +38,59 @@ export class GameService {
         newGame.users = [user];
         willSaved.push(newGame);
       }
-      await this.gameRepository.save(willSaved);
-      return { msg: '첫 게임 저장' };
-    }
+      try {
+        await this.gameRepository.save(willSaved);
+        return { msg: '첫 게임 저장' };
+      } catch (error) {
+        throw new InternalServerErrorException('DB에 게임 저장 실패');
+      }
+    } else {
+      const dbGameMap = new Map<number, Game>();
+      for (const dbGame of dbGames) {
+        dbGameMap.set(dbGame.appid, dbGame);
+      }
 
-    const dbGameMap = new Map<number, Game>();
-    for (const dbGame of dbGames) {
-      dbGameMap.set(dbGame.appid, dbGame);
-    }
+      /** db에 없는 게임 */
+      const notExistDbGames: Game[] = [];
 
-    /** db에 없는 게임 */
-    const notExistDbGames: Game[] = [];
+      /** db에 있는 게임 유저 업데이트*/
+      const updateDbGames: Game[] = [];
 
-    /** db에 있는 게임 유저 업데이트*/
-    const updateDbGames: Game[] = [];
-
-    for (const gameFromSteam of gamesFromSteam) {
-      const dbGame = dbGameMap.get(gameFromSteam.appid);
-      if (dbGame) {
-        // 유저가 db에 저장된 게임을 새로 산 경우 게임과 유저 연결
-        if (
-          !dbGame.users.some(
-            (alreadyUser) => alreadyUser.steamid === user.steamid,
-          )
-        ) {
-          dbGame.users.push(user);
-          updateDbGames.push(dbGame);
+      for (const gameFromSteam of gamesFromSteam) {
+        const dbGame = dbGameMap.get(gameFromSteam.appid);
+        if (dbGame) {
+          // 유저가 db에 저장된 게임을 새로 산 경우 게임과 유저 연결
+          if (
+            !dbGame.users.some(
+              (alreadyUser) => alreadyUser.steamid === user.steamid,
+            )
+          ) {
+            dbGame.users.push(user);
+            updateDbGames.push(dbGame);
+          }
+        } else {
+          // db에 게임이 존재하지 않는 경우
+          const newGames = new Game();
+          newGames.appid = gameFromSteam.appid;
+          newGames.name = gameFromSteam.name;
+          newGames.users = [user];
+          notExistDbGames.push(newGames);
         }
-      } else {
-        // db에 게임이 존재하지 않는 경우
-        const newGames = new Game();
-        newGames.appid = gameFromSteam.appid;
-        newGames.name = gameFromSteam.name;
-        newGames.users = [user];
-        notExistDbGames.push(newGames);
+      }
+
+      try {
+        if (notExistDbGames.length > 0) {
+          await this.gameRepository.save(notExistDbGames);
+        }
+        if (updateDbGames.length > 0) {
+          await this.gameRepository.save(updateDbGames);
+        }
+
+        return true;
+      } catch (error) {
+        throw new InternalServerErrorException('DB에 게임 업데이트 실패');
       }
     }
-
-    if (notExistDbGames.length > 0) {
-      await this.gameRepository.save(notExistDbGames);
-    }
-    if (updateDbGames.length > 0) {
-      await this.gameRepository.save(updateDbGames);
-    }
-
-    return true;
   }
 
   async findOwnedGame(steamid: string) {
@@ -97,6 +104,27 @@ export class GameService {
     const game = await this.gameRepository.findOne({
       where: { appid: gameId },
     });
+    if (!game) {
+      throw new NotFoundException('해당 게임은 DB에 없습니다.');
+    }
     return game;
+  }
+  //-------------------- Steam api -----------------
+
+  async getUserGamesFromSteam(steamid) {
+    try {
+      const steamApiKey = this.configService.get<string>('STEAM_API_KEY');
+      const { data } = await axios.get(
+        `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${steamApiKey}&steamid=${steamid}&include_appinfo=1`,
+      );
+
+      /** 스팀에서 가져온 해당 유저가 보유한 게임 목록 */
+      const gamesFromSteam = data.response.games;
+      return gamesFromSteam;
+    } catch (error) {
+      throw new ServiceUnavailableException(
+        '유저 소유 게임 리스트 스팀 api 실패',
+      );
+    }
   }
 }
